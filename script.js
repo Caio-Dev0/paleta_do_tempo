e7de691d6425fdc1b111a326e54ee6ef93133507
Caio Lucas <clucaszrtmain@gmail.com>
2025-11-05 00:15:05 -0300

    feature: função para mudar mensagem de boas vindas de acordo com cada horário do dia

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 let mensagem = document.querySelector('#msg')
+let mensagemChegada = document.querySelector("#msg-chegada")
 
-function carregar(){
+function carregarHoraAtual(){
     let dataAtual = new Date();
     let horaAtual = dataAtual.getHours();
     mensagem.textContent = `Agora são ${horaAtual} horas`
     return horaAtual
 }
 
-carregar()
+function mudaPaletaTempo(){
+    const horaAtual = carregarHoraAtual()
+    if(horaAtual >= 6 && horaAtual < 12){
+        mensagemChegada.textContent = "🌞 Bom dia, Flor do dia"
+        
+    }else if(horaAtual >= 12 && horaAtual < 18){
+        mensagemChegada.textContent = "😎 Boa tarde, Flor da tarde"
+    }else{
+        mensagemChegada.textContent = "😴 Boa noite, Flor da noite"
+    }
+}
+
+carregarHoraAtual()
+mudaPaletaTempo()
