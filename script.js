03610adf4388cedbf8b6c9086c7f02e56a58b5db
Caio Lucas <clucaszrtmain@gmail.com>
2025-11-05 22:47:17 -0300

    feature: Mudança do background do body de acordo com a hora do dia

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let mensagem = document.querySelector('#msg')
 let mensagemChegada = document.querySelector("#msg-chegada")
+let body = document.querySelector("body")
 
 function carregarHoraAtual(){
     let dataAtual = new Date();
@@ -12,11 +13,14 @@ function mudaPaletaTempo(){
     const horaAtual = carregarHoraAtual()
     if(horaAtual >= 6 && horaAtual < 12){
         mensagemChegada.textContent = "🌞 Bom dia, Flor do dia"
+        body.style.backgroundImage = "url(/assets/manha.jpg)"
         
     }else if(horaAtual >= 12 && horaAtual < 18){
         mensagemChegada.textContent = "😎 Boa tarde, Flor da tarde"
+         body.style.backgroundImage = "url(/assets/tarde.jpg)"
     }else{
         mensagemChegada.textContent = "😴 Boa noite, Flor da noite"
+        body.style.backgroundImage = "url(/assets/noite.jpg)"
     }
 }
 
